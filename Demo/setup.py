6f0aeb39047e941f0e3927a3d689dda006014576
VictorOntiverosR <156631200+VictorOntiverosR@users.noreply.github.com>
2024-03-26 21:49:14 -0400

    Update setup.py

--- Demo/setup.py
+++ Demo/setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup
 
+setup(
+    name= 'Demo',
+    version= '0.1',
+    description='A simple demonstration package',
+    url= 'https://github.com/VictorOntiverosR/M09',
+    author='Victor Ontiveros',
+    author_email = 'qfw3cr@virginia.edu',
+    licenses = 'MIT',
+    packages = ['demo'] 
+)
+  
+
+
+    
